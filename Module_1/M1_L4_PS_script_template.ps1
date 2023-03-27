8940906a677bf95ac1c8d0a87a29164d163d0347
nichtertamas <nichtertamas@gmail.com>
2023-03-27 20:43:14 +0200

    done

--- Module_1/M1_L4_PS_script_template.ps1
+++ Module_1/M1_L4_PS_script_template.ps1
@@ -9,4 +9,10 @@ $vmName = "VM1-Sales"
 $resourceGroup = "Sales"
 
 #Create Azure credentials
-$adminCredentials = Get-Credential -Message "Enter username "
+$adminCredentials = Get-Credential -Message "Enter username and PW"
+
+#Create new VM
+New-AzVM -ResourceGroupName $resourceGroup -Name $vmName -Credential $adminCredentials -Image UbuntuLTS
+
+#Get the new VM created
+Get-AzVM -ResourceGroupName $resourceGroup -Name $vmName | Format-Table
