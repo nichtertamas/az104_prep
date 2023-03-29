aeb87756b82662df2560091a1caf05582b9f4b1c
nichtertamas <nichtertamas@gmail.com>
2023-03-29 15:01:09 +0200

    commit

---
+++ Module 2/M2_L6_Users_Guests.ps1
@@ -0,0 +1,20 @@
+# create a new user
+New-AzureADUser
+
+# bulk creation of users
+$invitations = import-csv c:\bulkinvite\invitations.csv
+
+$messageInfo = New-Object Microsoft.Open.MSGraph.Model.InvitedUserMessageInfo
+
+$messageInfo.customizedMessageBody = "Hello. You are invited to the Contoso organization."
+
+foreach ($email in $invitations)
+   {New-AzureADMSInvitation `
+      -InvitedUserEmailAddress $email.InvitedUserEmailAddress `
+      -InvitedUserDisplayName $email.Name `
+      -InviteRedirectUrl https://myapps.microsoft.com `
+      -InvitedUserMessageInfo $messageInfo `
+      -SendInvitationMessage $true
+   }
+
+
